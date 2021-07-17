9cec7678838993f82a3e8f06bb90fef9da08db4e
Naitik <33337376+NaitikSanas@users.noreply.github.com>
2021-07-17 09:51:43 +0530

    file management

---
+++ main/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+
+void app_main(){
+    
+}
